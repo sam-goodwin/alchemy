1f71141295654a91afc65816f0dc7bd44955f56f
Zeb Piasecki <zeb@zebulon.dev>
2025-05-31 08:23:50 -0400

    fix: make global Env type for examples using CloudFlareEnv

--- examples/cloudflare-orange/app/env.ts
+++ examples/cloudflare-orange/app/env.ts
@@ -2,7 +2,6 @@ import type { website } from "../alchemy.run.js";
 
 export type CloudFlareEnv = typeof website.Env;
 
-// Necessary for the `env` property of loaders/actions to be typed
 declare global {
   export type Env = CloudFlareEnv;
 }
